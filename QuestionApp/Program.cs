7460c7e6d4e0d2d1e90d44ec2dd744505364b077
lmike <lmike@MLEE-AP>
2022-06-02 10:46:49 -0600

    Add project files.

---
+++ QuestionApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RestSharp;
+
+namespace QuestionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Ask your yes or no question:");
+                var question = Console.ReadLine();
+                var client = new RestClient("https://yesno.wtf/api");
+                var request = new RestRequest();
+
+                var reponse = client.ExecuteGetAsync<Data>(request).Result;
+
+                Console.WriteLine(reponse.Data.answer);
+
+                Console.WriteLine("Hit enter to ask another or CTRL+C to exit...");
+
+                Console.ReadLine();
+                Console.Clear();            }
+        }
+
+        public class Data
+        {
+            public string answer { get; set; }
+
+            public string imageUrl { get; set; }
+        }
+    }
+}
